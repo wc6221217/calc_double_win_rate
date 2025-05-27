7769176b88b842a788c683cefa487cc11f1509f3
曹魏 <caow@ct108.com>
2025-05-27 14:50:15 +0800

    Initial commit

---
+++ double_dealdata.py
@@ -0,0 +1,156 @@
+# encoding=utf-8
+import os,json,copy
+import multiprocessing as mp
+from collections import deque
+
+import pandas as pd
+# jqr_uid = pd.read_csv(r'E:\code\Server_Train\DDZ\ddz_analysis\robot_num\data\22042robotuid.csv')['uid'].to_list()
+def get_call_rob_data(file,i):
+    print(i,file)
+    all_data = []
+    # rob_all_data = []
+    with open(r'.\\calc_double_win_rate\\logs\\{}'.format(file), "r", encoding='gb18030',errors='ignore') as f:
+    # with open(r'D:\ddz_double\ddz_double\{}'.format(file), "r", encoding='gb18030',errors='ignore') as f:
+    # with open(r'E:\code\Server_Train\DDZ\ddz_bxp\datadeal\data\middle_data\game\zgdasvr\Record\{}'.format(file),
+    #           "r", encoding='gb18030', errors='ignore') as f:
+        f = f.readlines()
+        throw_chair = deque(maxlen=15)
+        banker = None
+        double_data = []
+        for i, line in enumerate(f):
+
+            line = line.replace('\n', '')
+            # 将读取的数据转为列表
+            linels = line.split(' ')
+
+            if linels[0] == 'Version':
+                #初始化
+
+                if banker != None:
+                    win_chair = int(throw_chair[-1])
+
+                    if win_chair == banker:
+                        for d_data in double_data:
+                            if d_data.get('chair_no') == d_data.get('banker'):
+                                d_data['is_win'] = 1
+                            else:
+                                d_data['is_win'] = 0
+                    else:
+                        for d_data in double_data:
+                            if d_data.get('chair_no') == d_data.get('banker'):
+                                d_data['is_win'] = 0
+                            else:
+                                d_data['is_win'] = 1
+
+                    all_data.extend(double_data)
+
+
+                double_data = []
+
+                hand_tile_dic = {0:'',1:'',2:''}
+                chairno_dic = {0:None,1:None,2:None}
+                is_robot_dict = {0: False, 1: False, 2: False}
+                call_label_dict = {0: -1, 1: -1, 2: -1}
+                rob_label_dict = {0: -1, 1: -1, 2: -1}
+                double_label_dict = {0: -1, 1: -1, 2: -1}
+
+                is_have_jqr = False
+
+            if linels[0] == 'Timestamp':
+                timestamp = linels[1]
+
+            is_have_jqr = True
+            if linels[0] == 'ChairNO':
+                #存储用户id
+                chairno_dic[int(linels[1])] = int(linels[2])
+            #     if int(linels[2]) in jqr_uid:
+            #         is_have_jqr = True
+            #         # is_robot_dict[int(linels[1])] = True
+
+            if linels[0] == 'Deal':
+                hand_tile_dic[int(linels[1])] = linels[2][1::2]
+
+            if linels[0] == 'Call':
+
+                call_label_dict[int(linels[1])] = int(linels[2])
+            if linels[0] == 'Rob':
+
+                rob_label_dict[int(linels[1])] = int(linels[2])
+
+
+            if linels[0] == 'Bottom':
+                bottom = linels[2][1::2]
+                hand_tile_dic[int(linels[1])] += linels[2][1::2]
+                banker = int(linels[1])
+
+
+            if linels[0].capitalize() == 'Double':
+                # if is_robot_dict[int(linels[1])] == False:
+                if is_have_jqr:
+                    # if int(linels[2]) != 0:
+
+                    if int(linels[2]) == 1:
+                        double_action = 2
+                    elif int(linels[2]) == 2:
+                        double_action = 4
+                    else:
+                        double_action = 1
+
+                    data = {
+                        'chair_no':int(linels[1]),
+                        'user_id':chairno_dic[int(linels[1])],
+                        'hand_tile': hand_tile_dic[int(linels[1])],
+                        'banker':banker,
+                        'bottom': bottom,
+                        'his_call': copy.deepcopy(call_label_dict),
+                        'his_rob': copy.deepcopy(rob_label_dict),
+                        'his_double': copy.deepcopy(double_label_dict),
+                        'double_action': double_action,
+                        'time':timestamp,
+                        'key':file[6:14]+'_'+str(timestamp)+'_'+str(chairno_dic[int(linels[1])])
+                    }
+                    double_data.append(data)
+
+                double_label_dict[int(linels[1])] = int(linels[2])
+
+            if linels[0] == 'Throw' or 'AutoThrow':
+                throw_chair.append(linels[1])
+
+    for index, data in enumerate(all_data):
+        data_dumps = json.dumps(data)
+
+        with open(r'.\\calc_double_win_rate\\{}.json'.format(file[:-7]), 'a', encoding='utf-8') as f:
+        # with open(r'D:\ddz_double\datadeal\{}.json'.format(file[:-7]), 'a', encoding='utf-8') as f:
+            json.dump(data_dumps, f)
+            f.write("\n")
+
+    # for index, data in enumerate(rob_all_data):
+    #     data_dumps = json.dumps(data)
+    #
+    #     with open(r'./call_rob_data/rob/{}.json'.format(file[:-7]), 'a', encoding='utf-8') as f:
+    #         json.dump(data_dumps, f)
+    #         f.write("\n")
+
+
+if __name__ == "__main__":
+
+    # log_files = os.listdir(r"E:\code\Server_Train\DDZ\ddz_bxp\datadeal\data\middle_data\game\zgdasvr\Record")
+    # log_files = os.listdir(r"D:\jqrdouble")
+    # log_files = os.listdir(r"E:\code\Server_Train\DDZ\ddz_analysis\robot_num\logs")
+    log_files = os.listdir(r".\\calc_double_win_rate\\logs")
+    select_log_files = [fille for fille in log_files if fille[:3] in ['188']]
+    # target_dir = 'E:\code\Server_Train\DDZ\ddz_analysis\\robot_num\double_data'
+    target_dir = '.\\calc_double_win_rate'
+
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    print(len(select_log_files))
+    pool = mp.Pool(4)
+    for i, log_file in enumerate(select_log_files):
+        pool.apply_async(get_call_rob_data,args=(log_file, i))
+
+
+    pool.close()
+    pool.join()
+    print("ok!")
---
+++ double_play_robot.py
@@ -0,0 +1,111 @@
+# 统计斗地主不洗牌 加倍功能  人机对局机器人和真人之间的对比
+# In[1]
+import pandas as pd
+# jqr_uid = pd.read_csv(r'E:\code\Server_Train\DDZ\ddz_analysis\robot_num\data\22042robotuid.csv')['uid'].to_list()
+# In[1]
+import json,os
+import pandas as pd
+# files_ls = os.listdir(r".\\calc_double_win_rate\\double_data")
+data_ls = []
+roomid = '188'
+
+double_data_play_ls = []
+double_data_robot_ls = []
+
+robot_version_data = {}
+new_jqr_uid = []
+with open(r'.\\calc_double_win_rate\\PlayRecord20250526.log', "r",encoding='gb18030', errors='ignore') as res_file:
+    next(res_file)
+    for line in res_file:
+        if len(line.strip()) == 0:
+            continue
+            
+        fields = line.strip().split(',')
+
+        role1 = fields[12]
+        role2 = fields[22]
+        role3 = fields[32]
+        uid1 = fields[9]
+        uid2 = fields[19]
+        uid3 = fields[29]
+        ai1 = fields[41]
+        ai2 = fields[42]
+        ai3 = fields[43]
+        timestamp = fields[44]
+
+
+        deposit1 = int(fields[13])
+        deposit2 = int(fields[23])
+        deposit3 = int(fields[33])
+
+        
+        #过滤掉有玩家参与的局
+        if len(ai1)==0 or len(ai2)==0 or len(ai3)==0:
+            continue
+        #过滤掉全部是相同机器人版本的局
+        if ai1 == ai2 and ai2 == ai3:
+            continue
+        #过滤掉没有胜负关系的局
+        if int(deposit1)==0:
+            continue
+
+        # print(deposit1, deposit2, deposit3)
+        if ai1 == '192.168.102.45:2412':
+            new_jqr_uid.append(timestamp+'_'+uid1)
+        if ai2 == '192.168.102.45:2412':
+            new_jqr_uid.append(timestamp+'_'+uid2)
+        if ai3 == '192.168.102.45:2412':
+            new_jqr_uid.append(timestamp+'_'+uid3)
+
+with open(r'.\\calc_double_win_rate\\188_20250526.json', "r",
+            encoding='gb18030', errors='ignore') as f:
+    f = f.readlines()
+    duiju_ls = []
+    for i, line in enumerate(f):
+        data = json.loads(json.loads(line))
+
+        if len(data.get('hand_tile')) == 20:
+            data['banker'] = 1
+        else:
+            data['banker'] = 0
+        time_uid_key = str(data.get('time'))+'_'+str(data.get('user_id'))
+        if time_uid_key in new_jqr_uid:
+            double_data_robot_ls.append(data)
+        else:
+            double_data_play_ls.append(data)
+
+
+double_play_df = pd.DataFrame(double_data_play_ls)
+double_robot_df = pd.DataFrame(double_data_robot_ls)
+# In[1]
+double_play_df_dz = double_play_df[double_play_df['banker']==1]
+double_play_df_nm = double_play_df[double_play_df['banker']==0]
+
+double_robot_df_dz = double_robot_df[double_robot_df['banker']==1]
+double_robot_df_nm = double_robot_df[double_robot_df['banker']==0]
+# In[1]
+double_play_df_dz_lose = double_play_df_dz[double_play_df_dz['is_win'] == 0]
+double_play_df_dz_win = double_play_df_dz[double_play_df_dz['is_win'] == 1]
+
+double_play_df_nm_lose = double_play_df_nm[double_play_df_nm['is_win'] == 0]
+double_play_df_nm_win = double_play_df_nm[double_play_df_nm['is_win'] == 1]
+# In[1]
+double_robot_df_dz_lose = double_robot_df_dz[double_robot_df_dz['is_win'] == 0]
+double_robot_df_dz_win = double_robot_df_dz[double_robot_df_dz['is_win'] == 1]
+
+double_robot_df_nm_lose = double_robot_df_nm[double_robot_df_nm['is_win'] == 0]
+double_robot_df_nm_win = double_robot_df_nm[double_robot_df_nm['is_win'] == 1]
+
+double_robot_df_dz_no_double = double_robot_df_dz[double_robot_df_dz['double_action'] == 1]
+double_robot_df_dz_double = double_robot_df_dz[double_robot_df_dz['double_action'] == 2]
+double_robot_df_nm_super_double = double_robot_df_nm[double_robot_df_nm['double_action'] == 4]
+
+#统计机器人不加倍、加倍和超级加倍情况下的胜率
+print("机器人不加倍的胜率：{:.2%}".format(len(double_robot_df_dz_no_double['is_win']==1)/len(double_robot_df_dz_no_double)))
+print("机器人加倍的胜率：{:.2%}".format(len(double_robot_df_dz_double['is_win']==1)/len(double_robot_df_dz_double)))
+print("机器人超级加倍的胜率：{:.2%}".format(len(double_robot_df_nm_super_double['is_win']==1)/len(double_robot_df_nm_super_double)))
+
+
+
+
+
